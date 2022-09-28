d02831d745bd342713c16d00c248eb04d4a0536c
danielrgbe <janrodion@gmail.com>
2022-09-28 03:01:20 -0500

    inico de la clase

---
+++ Chapter_10/exercise_6/BaseballGame.java
@@ -0,0 +1,19 @@
+//import java.util.Scanner;
+
+public class BaseballGame
+{
+	//static Scanner teclado = new Scanner(System.in);
+
+	public static void main(String[] args)
+	{
+
+		System.out.println("Hola bash");
+
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "todo ok";
+	}
+}
---
+++ Chapter_10/exercise_6/BaseballTeam.java
@@ -0,0 +1,16 @@
+public class BaseballTeam
+{
+	private String teamName;
+	private int[] iningScore;
+
+	public BaseballTeam(int inings)
+	{
+		iningScore = new int[inings];
+	}
+
+	@Override
+	public String toString()
+	{
+		return "todo ok";
+	}
+}
